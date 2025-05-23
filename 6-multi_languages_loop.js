3663cf60a899a5712148692bf182324abb42e158
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-23 19:46:31 +0300

    Add 6-multi_languages_loop.js file to print sentence using a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// A script that prints 3 lines
+
+const sentence = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < sentence.length; i++) {
+  console.log(sentence[i]);
+} 
+
+
