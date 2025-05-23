474f7e27710c5e71b7002f6c6520d7ae7abbe422
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-23 22:32:48 +0300

    Add 7-multi_c.js file

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+// A script that prints x times "C is fun"
+
+let sentence = parseInt(process.argv[2]);
+
+if (Number.isNaN(sentence)) {
+    console.log("Missing number of occurrences");
+} else {
+for (let x = 0; x < sentence; x++) {
+    console.log("C is fun");
+ }
+}
