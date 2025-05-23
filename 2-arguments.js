d7e53af363c8a7a05e1b28af79a0fcadb7eff289
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-23 12:43:22 +0300

    Add 2-arguments.js file

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+// A script that prints a message depending on the number of arguments passed
+
+const arguments = process.argv.slice(2)
+
+if (arguments.length === 0) { 
+console.log("No argument");
+} else if (arguments.length === 1) {
+console.log("Argument found");
+} else {
+console.log("Arguments found");
+}
