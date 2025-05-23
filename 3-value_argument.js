ad14a44e36581345beb65f6c375e957f004fd4a9
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-23 13:18:43 +0300

    Add a 3-value_argument.js file to print argument

---
+++ 3-value_argument.js
@@ -0,0 +1,10 @@
+// A script that prints the first argument passed to it
+
+const arguments = process.argv[2];
+
+if (arguments === undefined) {
+console.log("No argument");
+} else {
+console.log(arguments);
+}
+
