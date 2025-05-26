ba9e86cf5c4b412275d8a8f786328c845d867bc2
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-26 11:04:27 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,9 +3,10 @@
 // A script that prints an integer
 
 const arguments = process.argv[2];
+const num = parseInt(arguments);
 
-if (Number.isInteger(arguments)) {
+if (Number.isNaN(num)) {
 console.log('Not a number'); 
 } else {
-console.log('My number:', arguments)
+console.log('My number:', num)
 }
